8e91b0243e47bdf60d69ef7c8001a227bb1307ae
David Fischer <fiscd@seas.upenn.edu>
2024-10-01 09:40:37 -0400

    build

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import * as Plot from "@observablehq/plot";
-import PlotFigure from "./PlotFigure.js";
+import PlotFigure from "./PlotFigure.jsx";
+import "./styles.css";
 import penguins from "./penguins.json";
+import timeseries from "./timeseries.json";
 
 export default function App() {
   return (
-    <div>
-      <h1>Penguins</h1>
+    <div className="App">
+      <h1>Plot + React</h1>
+      <h2>Penguins scatterplot</h2>
       <PlotFigure
         options={{
           marks: [
-            Plot.dot(penguins, {x: "culmen_length_mm", y: "culmen_depth_mm"})
+            Plot.dot(penguins, { x: "culmen_length_mm", y: "culmen_depth_mm" })
           ]
+        }}
+      />
+      <h2>Time-series bar chart</h2>
+      <PlotFigure
+        options={{
+          x: { tickFormat: "d", interval: 1 },
+          marks: [Plot.barY(timeseries, { x: "year", y: "population" })]
         }}
       />
     </div>
